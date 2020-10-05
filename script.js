7c76435b091b73446167d1b1762a3013f77104a7
Aaron Lu <aaronnoh0409@gmail.com>
2020-10-04 20:14:22 -0400

    added dom elements and objects to js

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-//assign objects to functions
+// Dom Elements
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboard = document.getElementById('clipboard');
+
+// Assign objects to functions
+const randomFunc = {
+  lower: getRandomLower,
+	upper: getRandomUpper,
+	number: getRandomNumber,
+	symbol: getRandomSymbol
+}
 
 
 
-
-//creating functions that generates random strings
+// Creating functions that generates random strings
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97); 
 }
