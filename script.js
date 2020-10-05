2bbd11964304ca4a15d9fec25ea13872746aff91
Aaron Lu <aaronnoh0409@gmail.com>
2020-10-04 20:05:26 -0400

    made functions for the inputs and fixed max and min for the password lenght

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+//assign objects to functions
+
+
+
+
+//creating functions that generates random strings
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97); 
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65); 
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48); 
+}
+
+function getRandomSymbol() {
+	const symbols = '!@#$%^&*(){}[]=<>/,.'
+	return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
